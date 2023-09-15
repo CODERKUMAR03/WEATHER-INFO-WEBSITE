1fcfa8b25175e23a2db66419f7c6d410b3026e20
MOHAN KUMAR <139258712+CODERKUMAR03@users.noreply.github.com>
2023-09-15 07:22:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function tc() {
+    document.getElementById("grand").style.backgroundColor = "lightblue";
+       }
+   function tc1() {
+    document.getElementById("grand").style.backgroundColor = "gray";
+       }
+   function tc2() {
+    document.getElementById("grand").style.backgroundColor = "yellow";
+       }
+   function tc3() {
+    document.getElementById("grand").style.backgroundColor = "pink";
+       }
+   function tc4() {
+    document.getElementById("grand").style.backgroundColor = "lightgreen";
+       }
+    function tc5() {
+        document.getElementById("grand").style.backgroundColor = "#d9ee5f";
+     }
+    function tc6() {
+            document.getElementById("grand").style.backgroundColor = "#8b8bae";
+      }
